5940dfcb5fa9b5ad582f30dd44d5ad134f1fa748
zahralmosawi <zalmosawii65@gmail.com>
2025-07-13 17:06:06 +0300

    add event listener for play buttons

--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
 function init(){
 const colors = ["Red","Green","Yellow","Blue","Purple","Pink"]
-const playBtn = document.querySelector('#play')
-const roundElem = document.querySelector('#round')
-const timerElem = document.querySelector('#timer')
-const scoreElem = document.querySelector('#score')
-const wordDisplay = document.querySelector('#words')
-const colorsOptions = document.querySelector('#colors')
-const redColor = document.querySelector('#red')
-const startScreen = document.querySelector('#start-screen')
-const gameScreen = document.querySelector('#game-screen')
-const endScreen = document.querySelector('#end-screen')
-const finalScore = document.querySelector('#final-score')
-const playAgainBtn = document.querySelector('#play-again')
-
 let round = 1
 let timeLeft = 1.0
 let score = 0
 let curretColor
+
+//DOM elements
+const startScreen = document.querySelector('#start-screen')
+const gameScreen = document.querySelector('#game-screen')
+const endScreen = document.querySelector('#end-screen')
+
+const playBtn = document.querySelector('#play')
+const playAgainBtn = document.querySelector('#play-again')
+
+const roundElem = document.querySelector('#round')
+const timerElem = document.querySelector('#timer')
+const scoreElem = document.querySelector('#score')
+const wordDisplay = document.querySelector('#words')
+const finalScore = document.querySelector('#final-score')
+
+const colorsBtns = document.querySelectorAll('.circle-btn')
+
+//start the game
+playBtn.addEventListener('click', startGame)
+playAgainBtn.addEventListener('click', startGame)
+
 }
 addEventListener('DOMContentLoaded', init)
